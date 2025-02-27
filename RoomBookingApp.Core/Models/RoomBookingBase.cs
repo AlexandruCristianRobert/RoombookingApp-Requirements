f9a4fb6d2eb7891914fdf231b655141e9175b148
Cristian Alexandru <cristian.alexandru@spark-radiance.com>
2025-02-27 16:26:15 +0200

    Integration

--- RoomBookingApp.Core/Models/RoomBookingBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RoomBookingApp.Core.Models
-{
-    public abstract class RoomBookingBase
-    {
-        public string FullName { get; set; } = default!;
-        public string Email { get; set; } = default!;
-        public DateTime Date { get; set; }
-    }
-}
---
+++ RoomBookingApp.Persistence.Tests/RoomBookingServiceTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoomBookingApp.Domain;
+using RoomBookingApp.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomBookingApp.Persistence.Tests
+{
+    public class RoomBookingServiceTest
+    {
+        public RoomBookingServiceTest()
+        {
+            var dbOptions = new DbContextOptionsBuilder<RoomBookingAppDbContext>().UseInMemoryDatabase("AvailableRoomTest")
+               .Options;
+        }
+
+        [Fact]
+        public void Should_Return_Available_Rooms()
+        {
+            var date = new DateTime(2025, 02, 27);
+
+            var dbOptions = new DbContextOptionsBuilder<RoomBookingAppDbContext>().UseInMemoryDatabase("AvailableRoomTest")
+                .Options;
+
+            using var context = new RoomBookingAppDbContext(dbOptions);
+            context.Add(new Room { Id = 1, Name = "Room 1" });
+            context.Add(new Room { Id = 2, Name = "Room 2" });
+            context.Add(new Room { Id = 3, Name = "Room 3" });
+
+            context.Add(new RoomBooking { RoomId = 1, FullName="RoomBooking 1", Email = "email-1@emai.com", Date = date });
+            context.Add(new RoomBooking { RoomId = 2, FullName = "RoomBooking 2", Email = "email-2@emai.com", Date = date.AddDays(-1) });
+
+            context.SaveChanges();
+
+            var roomBookingService = new RoomBookingService(context);
+
+            //Act
+            var availableRooms = roomBookingService.GetAvailableRooms(date);
+
+            //Assert
+            Assert.Equal(2, availableRooms.Count());
+            Assert.Contains(availableRooms, q => q.Id == 2);
+            Assert.Contains(availableRooms, q => q.Id == 3);
+            Assert.DoesNotContain(availableRooms, q => q.Id == 1);
+        }
+
+        [Fact]
+        public void Should_Save_Room_Booking()
+        {
+            var dbOptions = new DbContextOptionsBuilder<RoomBookingAppDbContext>().UseInMemoryDatabase("AvailableRoomTest")
+               .Options;
+
+            var roomBooking = new RoomBooking { RoomId = 1, Date = new DateTime(2025, 02, 27), Email = "test-1@email.com", FullName = "Test" };
+
+            using var context = new RoomBookingAppDbContext(dbOptions);
+            var roomBookingService = new RoomBookingService(context);
+            roomBookingService.Save(roomBooking);
+
+            var bookings = context.RoomBookings.ToList();
+            var booking = Assert.Single(bookings);
+
+            Assert.Equal(roomBooking.Date, booking.Date);
+            Assert.Equal(roomBooking.RoomId, booking.RoomId);
+
+
+
+        }
+    }
+}
