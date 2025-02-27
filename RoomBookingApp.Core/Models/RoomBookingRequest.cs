f392813d49a65102b09a05d11bc1c30513237ce2
Cristian Alexandru <cristian.alexandru@spark-radiance.com>
2025-02-27 13:41:11 +0200

    Add project files.

---
+++ RoomBookingApp.Core/Models/RoomBookingRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace RoomBookingApp.Core.Models
+{
+    public class RoomBookingRequest
+    {
+        public string FullName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public DateTime Date { get; set; }
+    }
+}
---
+++ RoomBookingApp.Core/Models/RoomBookingResult.cs
@@ -0,0 +1,9 @@
+﻿namespace RoomBookingApp.Core.Models
+{
+    public class RoomBookingResult
+    {
+        public string FullName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public DateTime Date { get; set; }
+    }
+}
