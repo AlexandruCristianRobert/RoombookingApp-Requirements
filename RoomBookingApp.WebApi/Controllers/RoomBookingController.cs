bf9481bee58fdcf18d4c523a754add0785f21e83
Cristian Alexandru <cristian.alexandru@spark-radiance.com>
2025-02-27 20:34:01 +0200

    temp

---
+++ RoomBookingApp.WebApi/Controllers/RoomBookingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+
+namespace RoomBookingApp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoomBookingController : ControllerBase
+    {
+        private IRoomBookingRequestProcessor _roomBookingProcessor;
+
+        public RoomBookingController(IRoomBookingRequestProcessor roomBookingProcessor)
+        {
+            _roomBookingProcessor = roomBookingProcessor;
+        }
+
+        public async Task<IActionResult> BookRoom(RoomBookingRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
